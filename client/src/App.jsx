e85b751388db5b5114fdb6c641575ce1ec8af03d
chhavijeiff <chhavijeiff@gmail.com>
2024-03-11 17:44:35 +0530

    Routers created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Browser from './Browser/Browser'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Pen & Pixel</h1>
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/browser' element={<Browser />}></Route>
+    </Routes>
     </>
   )
 }
