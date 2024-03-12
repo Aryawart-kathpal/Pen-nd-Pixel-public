0311a4daca6a70d36de4143f2493217341682790
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2024-03-12 17:22:16 +0530

    Upload-profile-pic Completed

--- server/app.js
+++ server/app.js
@@ -14,10 +14,22 @@ const cookieParser = require('cookie-parser');
 const notFoundMiddleware= require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+//Other packages
+const fileUpload = require('express-fileupload');
+
+//Cloudinary
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret:process.env.CLOUD_API_SECRET,
+})
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET))
 app.use(morgan('tiny'));
+app.use(fileUpload({useTempFiles:true}));
 
 //Importing Routes
 const authRouter=require('./routes/authRoutes');
@@ -38,3 +50,6 @@ const start = async()=>{
 }
 
 start();
+
+//get Current user and it's analytics such as all his notes, as well as for a note ratings comments,etc. have also to be made in a model
+// also some review,contact us,etc. have to be thought of on some later stage, along with setting some pre and post aggregation pipeline for the ratings,etc,
