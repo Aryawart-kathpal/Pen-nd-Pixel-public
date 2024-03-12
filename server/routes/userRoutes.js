c9222e70467d1aed23660af9d7fd2887d74149bd
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2024-03-12 18:11:17 +0530

    User Controller setup

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router = express.Router();
+
+const {authorizePermissions,authenticateUser}=require('../middleware/authentication');
+const{getAllUsers,getSingleUser,getCurrentUser,updateUser,deleteUser,updateUserPassword}=require('../controllers/userController');
+
+router.route('/').get([authenticateUser,authorizePermissions('admin')],getAllUsers);
+router.route('/current').get(authenticateUser,getCurrentUser);
+router.route('/update').patch(authenticateUser,updateUser);// only same user
+router.route('/update-password').patch(authenticateUser,updateUserPassword);// only the logged in user can change it's own password
+router.route('/delete/:id').delete(authenticateUser,deleteUser);// only admin and same user -> checkPermissions
+router.route('/:id').get([authenticateUser,authorizePermissions('admin')],getSingleUser);//only for admin, when user wants to access it's own then, getCurrentUser will be used
+
+module.exports=router;
