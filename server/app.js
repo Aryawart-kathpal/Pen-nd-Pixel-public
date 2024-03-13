bec51fe9bbbcb448b334ea037ba6fc698961c1ce
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2024-03-14 02:11:20 +0530

    Follow Unfollow Functionallity complete

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 //Importing Routes
 const authRouter=require('./routes/authRoutes');
 const userRoutes=require('./routes/userRoutes');
+const followRoutes=require('./routes/followRoutes');
 
 //Other packages
 const fileUpload = require('express-fileupload');
@@ -38,6 +39,10 @@ app.use(fileUpload({useTempFiles:true}));
 //Routes
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/user',userRoutes);
+app.use('/api/v1/user',followRoutes);
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
@@ -52,6 +57,8 @@ const start = async()=>{
 }
 
 start();
+// follow settings have to be made in the user model only, numOfFollowers and List Of Followers have to be made in the user model only -> aggregate pipleine has to be made for realtime updating the followers and following, also list of following and numOfFollowing has to be made too
+// authenticateUser to be stick at the follow also
 
 // sequence-> now firstly userController has to be setup for now and then the notesController with CRUD and other pipeline,etc. have to be setup at the end may be review,comments have to be thought of along with rating of a note
 
