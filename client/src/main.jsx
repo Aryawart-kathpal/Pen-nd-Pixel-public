85346699d49c37f6d2372f339504155a0c582de3
Debatreya <debatreyadas@gmail.com>
2024-03-18 01:12:07 +0530

    Light and Dark mode added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./Context/ThemeProvider";
 
-ReactDOM.createRoot(document.getElementById('Pen-And-Pixel')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    
-)
+ReactDOM.createRoot(document.getElementById("Pen-And-Pixel")).render(
+	<BrowserRouter>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</BrowserRouter>
+);
