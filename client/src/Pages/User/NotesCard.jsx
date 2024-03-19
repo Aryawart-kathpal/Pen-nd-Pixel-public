31dbf0f06d1c0d85e0ed05086cb9439ef06f218b
Muskan-9090 <ms9254590@gmail.com>
2024-03-19 10:06:08 +0530

    Changes in UserDashboard

--- client/src/Pages/User/NotesCard.jsx
+++ client/src/Pages/User/NotesCard.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 
-function Notes({title, desciption, likes, comments}){
-    console.log(title, desciption, likes, comments);
-    return(
-        <div className="border border-black rounded-sm basis-[100%]">
-            <div>
-                {title}
-            </div>
+function Notes({ title, description, likes, comments }) {
+  console.log(title, description, likes, comments);
+  return (
+    <div className="shadow-sm shadow-black rounded-md px-4 pt-3 min-w-[90%] max-w-[90%] mx-auto">
+      <h2 className="text-lg text-black font-bold">{title}</h2>
+      <p className="text-gray-600 whitespace-nowrap text-ellipsis overflow-hidden">
+        {description}
+      </p>
+      <div className="flex justify-between items-center pt-1">
+        <div></div>{" "}
+        <div className="flex items-center space-x-2">
+          <div className="flex">
+            <img
+              src="https://i.pinimg.com/564x/c6/7d/a0/c67da01511fe76591011a81094846e87.jpg"
+              alt="Likes Icon"
+              className="w-4 h-5 sm:w-6 sm:h-6"
+            ></img>
+            <p className="text-sm sm:text-base">{likes}</p>
+          </div>
+          <div className="flex">
+            <img
+              src="https://www.pngitem.com/pimgs/m/584-5843167_icon-of-comment-in-white-hd-png-download.png"
+              alt="Comments Icon"
+              className="w-4 h-5 sm:w-6 sm:h-6"
+            ></img>
+            <p className="text-sm sm:text-base">{comments.length}</p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-export default Notes
+export default Notes;
--- client/src/Pages/User/UserDashboard.jsx
+++ client/src/Pages/User/UserDashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Profile from "./Profile";
 import Notes from "./NotesCard";
+import Button from "../../Components/Button";
+import { FaBars } from "react-icons/fa6";
 
 function UserDasboard() {
   const [notes, setNotes] = useState([
@@ -47,24 +49,75 @@ function UserDasboard() {
         },
       ],
     },
+    {
+      id: 4,
+      title: "Note 4",
+      description: "Description of Note 4",
+      likes: "80",
+      comments: [
+        {
+          name: "Alex Flex",
+          text: "Best Note ever",
+        },
+        {
+          name: "Alex Flex",
+          text: "Best Note ever",
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Note 5",
+      description: "Description of Note 5",
+      likes: "20",
+      comments: [
+        {
+          name: "Alex Flex",
+          text: "Best Note ever",
+        },
+        {
+          name: "Alex Flex",
+          text: "Best Note ever",
+        },
+        {
+          name: "Alex Flex",
+          text: "Best Note ever",
+        },
+      ],
+    },
+    {
+      id: 6,
+      title: "Note 6",
+      description: "Description of Note 6",
+      likes: "0",
+      comments: [
+        {
+          name: "Alex Flex",
+          text: "Best Note ever",
+        },
+      ],
+    },
   ]);
   return (
-    <div className="flex">
+    <div className="flex max-h-screen">
+      {/* <FaBars /> */}
       <Profile />
-      <div className="flex flex-col items-center basis-[100%] p-4">
-        <div className="banner bg-slate-600 text-white self-stretch h-52 flex items-center justify-center relative rounded-lg text-5xl font-semibold">
+      <div className="flex flex-col items-center basis-[100%] p-4 ">
+        <div className="banner bg-slate-800 text-white self-stretch h-48 flex items-center justify-center relative rounded-lg text-5xl font-semibold mb-2">
           MY NOTES
         </div>
-        <div className="flex flex-col items-start gap-5">
+
+        <div className="grid w-full gap-5 overflow-x-hidden overflow-y-scroll customScrollbar py-5 pt-3 max-h-[65%] sticky ">
           {notes.map((note) => {
-            return (
-              <Notes
-                {...note}
-                key={note.id}
-              />
-            );
+            return <Notes {...note} key={note.id} />;
           })}
         </div>
+        <div className=" flex justify-center mt-4">
+              <Button
+                text="Create new note"
+                className="bg-slate-800 text-white p-2 border-2 rounded border-black"
+              />
+            </div>
       </div>
     </div>
   );
