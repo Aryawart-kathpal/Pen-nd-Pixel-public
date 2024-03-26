4537c828544fa30759830872d50133438e0f2b00
Debatreyta <debatreyadas@gmail.com>
2024-03-26 18:37:04 +0530

    Working on the readers flow

--- client/src/Components/BrowserCard.jsx
+++ client/src/Components/BrowserCard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useNavigation } from "react-router-dom";
 
-function Card ({title, description}){
-      console.log({title});
-      console.log({description});
+function Card ({title, description, topics, content, authorDetails}){
+      const navigate = useNavigation()
+      const data = {
+        title,
+        content,
+        topics,
+        authorDetails
+      }
       return (
         <>
-          <div className="shadow-sm shadow-black rounded-md px-4 py-3 min-w-[90%] max-w-[90%] mx-auto">
-            <h2 className="text-lg text-black font-bold">{title}</h2>
+          <div 
+            className="shadow-sm shadow-black rounded-md px-4 py-3 min-w-[90%] max-w-[90%] mx-auto cursor-pointer" 
+            onClick={() => navigate("/blog", {state: {...data}})}>
+            <h2 className="text-lg font-bold">{title}</h2>
             <p className="text-gray-600 whitespace-nowrap text-ellipsis overflow-hidden">{description}</p>
           </div>
         </>
--- client/src/Pages/Browser/Browser.jsx
+++ client/src/Pages/Browser/Browser.jsx
@@ -7,36 +7,9 @@ import Nav from '../../Layouts/Nav.jsx'
 
 const Browser = () => {
   const navigate = useNavigate()
-  // const useDebounce = (value, delay) => {
-  //   const [debouncedValue, setDebouncedValue] = useState(value);
-  
-  //   useEffect(() => {
-  //     const handler = setTimeout(() => {
-  //       setDebouncedValue(value);
-  //     }, delay);
-  
-  //     return () => {
-  //       clearTimeout(handler);
-  //     };
-  //   }, [value, delay]);
-  
-  //   return debouncedValue;
-  // };
-  // const [searchText, setSearchText] = useState('');
-  // const [filteredCards, setFilteredCards] = useState([]);
-
-  // const debouncedSearchText = useDebounce(searchText, 300);
-
-  // useEffect(() => {
-  //   const filteredCards = cardsData.filter(card =>
-  //     card.title.toLowerCase().includes(debouncedSearchText.toLowerCase())
-  //   );
-  //   setFilteredCards(filteredCards);
-  // }, [cardsData, debouncedSearchText]);
-
   const [searchText, setSearchText] = useState()
   const fetchData = async () => {
-
+    // CODE
   }
   useEffect(()=>{
     fetchData();
@@ -45,37 +18,72 @@ const Browser = () => {
     {
       id: "1",
       title: "Title 1",
-      description: "description 1"
+      description: "description 1",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails:{
+        authorName: 'Author'
+      }
     },
     {
       id: "2",
       title: "Title 2",
-      description: "description 2"
+      description: "description 2",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails: {
+        authorName: 'Author'
+      }
     },
     {
       id: "3",
       title: "Title 3",
-      description: "description 3"
+      description: "description 3",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails: {
+        authorName: 'Author'
+      }
     },
     {
       id: "4",
       title: "Title 4",
-      description: "description 4"
+      description: "description 4",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails: {
+        authorName: 'Author'
+      }
     },
     {
       id: "5",
       title: "Title 5",
-      description: "hdvsdcjhsdvhcjvsdvcvjsdvcvsdvjcvsjhdvchksvdhcvshvcshvcjkvscksdvchkvshjvcjvhjsvcjhshshjcvladcvjvjcsdbcvhjdvhjdjvbcj"
+      description: "hdvsdcjhsdvhcjvsdvcvjsdvcvsdvjcvsjhdvchksvdhcvshvcshvcjkvscksdvchkvshjvcjvhjsvcjhshshjcvladcvjvjcsdbcvhjdvhjdjvbcj",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails: {
+        authorName: 'Author'
+      }
     },
     {
       id: "6",
       title: "Title 6",
-      description: "description 6"
+      description: "description 6",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails: {
+        authorName: 'Author'
+      }
     },
     {
       id: "7",
       title: "Title 7",
-      description: "description 7"
+      description: "description 7",
+      topics: ['frontend', 'react'],
+      content: `<html><title></title><body><h1>Title</h1><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eaque tenetur asperiores maiores atque ipsa sint! Molestias sint, corporis natus delectus optio quibusdam, ea quae iste, voluptatem accusantium explicabo exercitationem.</p></body></html>`,
+      authorDetails: {
+        authorName: 'Author'
+      }
     }
 
   ])
@@ -85,18 +93,18 @@ const Browser = () => {
   return (
     <>
       <Nav />
-      <div className="shadow-lg shadow-black text-white max-h-[82svh] rounded-md  flex flex-col  gap-8 sm:max-w-[60svw] mx-auto my-5 p-4 items-center">
+      <div className="shadow-lg shadow-black max-h-[82svh] rounded-md  flex flex-col  gap-8 sm:max-w-[60svw] mx-auto my-5 p-4 items-center">
         <IoIosArrowBack 
-          className="text-3xl absolute text-black left-10 max-sm:hidden cursor-pointer" 
+          className="text-3xl absolute left-10 max-sm:hidden cursor-pointer" 
           onClick={() => navigate(-1)}
         />
-        <div className="flex  gap-4 items-center w-full  mt-5 relative">
+        <div className="flex gap-4 items-center w-full sm:max-w-[720px] mt-5 relative">
             <input
             type="text"
             placeholder="Search..."
             value={searchText}
             onChange={handleSearch}
-            className="bg-white text-black rounded px-4 py-2 outline w-full sm:self-stretch overflow-x-hidden"
+            className="rounded px-4 py-2 outline w-full sm:self-stretch overflow-x-hidden"
             />
           <button className="bg-black text-white rounded px-4 py-2 sm:mt-0  sm:w-auto">Search</button>
         </div>
@@ -106,6 +114,9 @@ const Browser = () => {
               key={data.id}
               title={data.title}
               description={data.description}
+              topics={data.topics}
+              content={data.content}
+              authorDetails={data.authorDetails}
               className="grid w-full gap-4"
               />
           ))}
