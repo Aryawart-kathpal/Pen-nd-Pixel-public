c01e1c906b2fe2e09f548a4a5dc0a7f66da300a7
Muskan-9090 <ms9254590@gmail.com>
2024-03-27 18:15:21 +0530

    Contact Us Page Created

--- client/src/Pages/Home/ContactUs.jsx
+++ client/src/Pages/Home/ContactUs.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
-
-export const ContactUs = () => {
+import React from "react";
+import Nav from "../../Layouts/Nav";
+import { FaHouse , FaPhoneFlip} from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+function ContactUs() {
   return (
-    <div>ContactUs</div>
-  )
+    <div>
+      <Nav />
+      <div
+        className="bg-cover bg-center bg-opacity-50 h-full"
+        style={{
+          backgroundImage:
+            "url(https://getwallpapers.com/wallpaper/full/7/c/b/335542.jpg)",
+        }}
+      >
+        <div className="p-14 flex flex-col items-center justify-center">
+          <h2 className="text-3xl text-white font-bold mb-3 text-center md:text-left ">
+            Contact Us
+          </h2>
+          <div className="mb-7 text-center md:text-left text-white">
+            <p>
+              We're here to help! Feel free to reach out to us using the form
+              below.
+            </p>
+          </div>
+          <div className="max-w-4xl bg-transparent rounded-lg shadow-lg flex flex-row md:flex-row">
+            <div className="md:w-1/2 flex flex-col justify-center items-center md:items-start text-white">
+              <div className="flex items-center mb-14">
+                <div className="mr-4">
+                  <FaHouse />
+                </div>
+                <div>
+                  <h3 className="font-semibold text-xl">Address</h3>
+                  <p>NIT Kurukshetra, Haryana</p>
+                </div>
+              </div>
+              <div className="flex items-center mb-14">
+                <div className="mr-4">
+                  <MdEmail />
+                </div>
+                <div>
+                  <h3 className="font-semibold text-xl">Email</h3>
+                  <p>penandpixel.com</p>
+                </div>
+              </div>
+              <div className="flex items-center mb-14">
+                <div className="mr-4">
+                  <FaPhoneFlip />
+                </div>
+                <div>
+                  <h3 className="font-semibold text-xl">Phone</h3>
+                  <p>+91 987654321</p>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-1/2 md:mt-0 bg-yellow-50 rounded-lg">
+              <div className="bg-opacity-50 p-8 rounded-lg shadow-md underline">
+                <form>
+                  <input
+                    type="text"
+                    placeholder="Full Name"
+                    className="w-full mb-6 p-2 rounded-lg bg-transparent border-b-2 border-gray-300 focus:border-gray-600"
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="w-full mb-6 p-2 rounded-lg bg-transparent border-b-2 border-gray-300 focus:border-gray-600"
+                  />
+                  <input
+                    type="tel"
+                    placeholder="Phone"
+                    className="w-full mb-6 p-2 rounded-lg bg-transparent border-b-2 border-gray-300 focus:border-gray-600"
+                  />
+                  <textarea
+                    placeholder="Type your Message..."
+                    rows="4"
+                    className="w-full mb-5 p-2 rounded-lg bg-transparent border-b-2 border-gray-300 focus:border-gray-600"
+                  ></textarea>
+                  <button
+                    type="submit"
+                    className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 mb-1 w-full text-md"
+                  >
+                    Send Message
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
