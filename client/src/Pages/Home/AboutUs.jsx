d9f31b4cf05af7dda15c33a629134e22f0f3e36f
Debatreyta <debatreyadas@gmail.com>
2024-03-30 14:33:16 +0530

    Tried to stop dance

--- client/src/Pages/Home/AboutUs.jsx
+++ client/src/Pages/Home/AboutUs.jsx
@@ -7,21 +7,21 @@ import MuskanImg from "../../assets/MuskanImg.jpg"
 
 const AboutUs = () => {
     return (
-        <section className='h-screen bg-black max-h-screen aboutUs p-4 flex flex-col justify-center' >
-            <h1 className='font-sans font-semibold'>Meet Our Team</h1>
-            <p className='sm:w-[60%] font-semibold'>Our team is a with dynamic group of undergraduates. With a compact but highly skilled workforce of four friends, we collabrate closely to drive innovation and deliver quality solutions. </p>
-            <div className="card-container min-h-[60svh] mt-auto">
-                <div id="image-container" className="card pl-4 pr-2 rounded-tl-lg">
-                    <img src={DebatreyaImg} alt="Debatreya" className='min-h-full' />
+        <section className='h-screen p-5 poppins-semibold flex flex-col overflow-hidden justify-between' >
+            <h1 className='font-semibold text-5xl'>Meet Our Team</h1>
+            <p className='py-3 sm:w-[60%]'>Our team is a with dynamic group of undergraduates. With a compact but highly skilled workforce of four friends, we collabrate closely to drive innovation and deliver quality solutions. </p>
+            <div className="card-container pt-20">
+                <div id="image-container" className="card">
+                    <img src={DebatreyaImg} alt="Debatreya" className='' />
                 </div>
-                <div id="image-container" className="card px-2">
-                    <img src={AryawartImg} alt="Aryawart" className='min-h-full' />
+                <div id="image-container" className="card">
+                    <img src={AryawartImg} alt="Aryawart" className='' />
                 </div>
-                <div id="image-container" className="card px-2">
-                    <img src={ChhaviImg} alt="Chhavi" className='min-h-full' />
+                <div id="image-container" className="card">
+                    <img src={ChhaviImg} alt="Chhavi" className='' />
                 </div>
-                <div id="image-container" className="card px-2 rounded-tr-lg">
-                    <img src={MuskanImg} alt="Muskan" className='min-h-full' />
+                <div id="image-container" className="card">
+                    <img src={MuskanImg} alt="Muskan" className='' />
                 </div>
                 
             </div>
