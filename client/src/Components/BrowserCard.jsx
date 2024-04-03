d39b3a3ba26a965402458512d102730c5e36a0c1
Muskan-9090 <ms9254590@gmail.com>
2024-04-03 14:24:33 +0530

    Category added in Browser Card

--- client/src/Components/BrowserCard.jsx
+++ client/src/Components/BrowserCard.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function Card ({id, title, description, topics, content, authorDetails}){
-      const navigate = useNavigate()
-      const data = {
-        title,
-        content,
-        topics,
-        authorDetails
-      }
-      return (
-          <div 
-            className="shadow-sm shadow-black rounded-md px-4 py-3 min-w-[90%] max-w-[90%] mx-auto cursor-pointer" 
-            onClick={() => navigate(`/blog/${id}`, {state: {...data}})}>
-            <h2 className="text-lg font-bold">{title}</h2>
-            <p className="text-gray-600 whitespace-nowrap text-ellipsis overflow-hidden">{description}</p>
-          </div>
-      )
+function Card({ id, title, description,category, topics, content, authorDetails }) {
+  const navigate = useNavigate();
+  const data = {
+    title,
+    content,
+    topics,
+    authorDetails,
+  };
+  return (
+    <div
+      className="shadow-sm shadow-black rounded-md px-4 py-3 min-w-[90%] max-w-[90%] mx-auto cursor-pointer "
+      onClick={() => navigate(`/blog/${id}`, { state: { ...data } })}
+    >
+      <div className="flex justify-between items-center">
+        <h2 className="text-lg font-bold">{title}</h2>
+        <div className="rounded-lg font-semibold px-2.5 py-0.5 border bg-gray-200 dark:text-black my-1 mr-2">
+          {category}
+        </div>
+      </div>
+      <p className="text-gray-500 whitespace-nowrap text-ellipsis overflow-hidden ">
+        {description}
+      </p>
+    </div>
+  );
 }
-export default Card
+export default Card;
