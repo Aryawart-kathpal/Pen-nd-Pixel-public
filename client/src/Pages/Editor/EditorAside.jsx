c7d5cdb44267a44185ddb254d561a59a3ca24636
Debatreyta <debatreyadas@gmail.com>
2024-04-05 00:05:49 +0530

    Create note page on progress

--- client/src/Pages/Editor/EditorAside.jsx
+++ client/src/Pages/Editor/EditorAside.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Toggle from "../../Components/Toggle";
 import { useTheme } from "../../Context/ThemeProvider";
+import { MdOutlineCreateNewFolder } from "react-icons/md";
+import { AiOutlineShareAlt } from "react-icons/ai";
+import { BsGraphUp } from "react-icons/bs";
+import { AiOutlineTeam } from "react-icons/ai";
+import { AiOutlineExport } from "react-icons/ai";
+import { AiOutlineCopy } from "react-icons/ai";
+
 
 const EditorAside = () => {
 	const { isDarkMode, toggleTheme } = useTheme();
@@ -19,23 +26,28 @@ const EditorAside = () => {
 			{/* Button for 1. Create Note 2. Share (open modal) 3. Dashboard 4. Start Collaboration 5. Export 6. Copy HTML */}
 			<div>
 				<div>
-					<span className="w-full h-12 border-b border-black">Create Note</span>
-				</div>
-				<div>
-					<span className="w-full h-12 border-b border-black">Share</span>
-				</div>
-				<div>
 					<span className="w-full h-12 border-b border-black">Dashboard</span>
 				</div>
-				<div>
+				<div className="flex items-center">
+          <MdOutlineCreateNewFolder className="w-6 h-6" />
+					<span className="w-full h-12 border-b border-black">Create Note</span>
+				</div>
+				<div className="flex items-center">
+          <AiOutlineShareAlt className="w-6 h-6" />
+					<span className="w-full h-12 border-b border-black">Share</span>
+				</div>
+				<div className="flex items-center">
+          <AiOutlineTeam className="w-6 h-6" />
 					<span className="w-full h-12 border-b border-black">
 						Start Collaboration
 					</span>
 				</div>
-				<div>
+				<div className="flex items-center">
+          <AiOutlineExport className="w-6 h-6" />
 					<span className="w-full h-12 border-b border-black">Export</span>
 				</div>
-				<div>
+				<div className="flex items-center">
+          <AiOutlineCopy className="w-6 h-6" />
 					<span className="w-full h-12 border-b border-black">Copy HTML</span>
 				</div>
 			</div>
