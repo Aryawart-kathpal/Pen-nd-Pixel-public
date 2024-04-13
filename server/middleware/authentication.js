b72c1afd3eed48bc62956d8c8e769c4a535ea626
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2024-04-14 01:24:03 +0530

    Share private note complete

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -2,13 +2,13 @@ const CustomError = require('../errors');
 const {isTokenValid, attachCookiesToResponse}= require('../utils');
 const Token = require('../models/Token');
 
-const authenticateUser = async(req,res,next)=>{
+const authenticateUser = async(req,res)=>{
     const {refreshToken,accessToken} =req.signedCookies;
     try {
         if(accessToken){// access Token exists mein that it is valid, as it's existing check is made in the controller only
             const payload = isTokenValid(accessToken);
             req.user=payload.user;
-            return next();
+            return ;
         }
         const payload = isTokenValid(refreshToken);
         
@@ -20,12 +20,13 @@ const authenticateUser = async(req,res,next)=>{
         if(!existingToken || !existingToken?.isValid){
             throw new CustomError.UnauthenticatedError('Authentication Invalid');
         }
-
+        // console.log("hello");
         attachCookiesToResponse({res,user:payload.user,refreshToken:payload.refreshToken});
         
         req.user=payload.user;
-        next();
+        // next(); -> had to remove this as in shareNote private it was giving error next() not defined
     } catch (error) {
+        console.log(error);
         throw new CustomError.UnauthenticatedError("Authentication Invalid");
     }
 }
