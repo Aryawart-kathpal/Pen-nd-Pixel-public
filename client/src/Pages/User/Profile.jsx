fd9b6f10f42d65c9eb58c0ef1d9410b18b2fe202
Debatreya <debatreyadas@gmail.com>
2024-04-15 03:33:45 +0530

    Edit profile

--- client/src/Pages/User/Profile.jsx
+++ client/src/Pages/User/Profile.jsx
@@ -1,41 +1,121 @@
 import React, { useState } from "react";
 import Button from "../../Components/Button";
 import { BiSolidHome } from "react-icons/bi";
-import {FaTimes} from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import axiosInstance from "../../Helpers/axiosInstance";
 import { useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useDisclosure } from "@chakra-ui/react";
+import { BsPersonCircle } from "react-icons/bs";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+} from "@chakra-ui/react";
 
-function Profile({handleToggle, toggle, user}) {
+function Profile({ handleToggle, toggle, user }) {
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	const navigate = useNavigate();
 	const toast = useToast();
-//   const [userData, setUserdata] = useState({
-//     name: "Random",
-//     img: "https://cdn-icons-png.flaticon.com/512/3177/3177440.png",
-//     followers: "10",
-//     following: "10",
-//     dateJoined: "13/03/2024",
-//     about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis lectus at leo malesuada volutpat ut ut eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis lectus at leo malesuada volutpat ut ut eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis lectus at leo malesuada volutpat ut ut eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-//   });
+	const [userData, setUserData] = useState(user);
+	const [previewImage, setPreviewImage] = useState("");
+	console.log(userData);
+	//   const [userData, setUserdata] = useState({
+	//     name: "Random",
+	//     img: "https://cdn-icons-png.flaticon.com/512/3177/3177440.png",
+	//     followers: "10",
+	//     following: "10",
+	//     dateJoined: "13/03/2024",
+	//     about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis lectus at leo malesuada volutpat ut ut eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis lectus at leo malesuada volutpat ut ut eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis lectus at leo malesuada volutpat ut ut eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	//   });
 
 	const handleLogout = async () => {
 		const res = axiosInstance.delete("/auth/logout");
 		toast.promise(res, {
-			loading: {title: "Logging out..."},
-			success: {title: "Logged out successfully"},
-			error: {title: "Error logging out"},
+			loading: { title: "Logging out..." },
+			success: { title: "Logged out successfully" },
+			error: { title: "Error logging out" },
 		});
 
 		localStorage.removeItem("user");
 		localStorage.setItem("isLoggedIn", false);
 
 		navigate("/");
+	};
+
+	const handleUserInput = (e) => {
+		const { name, value } = e.target;
+		setUserData({
+			...userData,
+			[name]: value,
+		});
+	};
+
+	function getImage(event) {
+		event.preventDefault();
+		//getting the image
+		const uploadedImage = event.target.files[0];
+		if (uploadedImage) {
+			setUserData({
+				...userData,
+				image: uploadedImage,
+			});
+			const fileReader = new FileReader();
+			fileReader.readAsDataURL(uploadedImage);
+			fileReader.addEventListener("load", function () {
+				setPreviewImage(this.result);
+			});
+		}
 	}
 
-	const [userData, setUserdata] = useState(user);
-	console.log(userData);
-  
-  return (
+	const handleEdit = async () => {
+		// If there is a new image upload image to a given router
+		if (userData.image) {
+			const formData = new FormData();
+			formData.append("image", userData.image);
+			const res = await axiosInstance.post("/auth/upload-image", formData);
+			console.log(res);
+			// Take the image url and update the user data
+			setUserData({
+				...userData,
+				image: res.data.url,
+			});
+		}
+		// Update the user details
+		try {
+			const res = axiosInstance.patch("/user/update", {
+				name: userData.name,
+				about: userData.about,
+				image: userData.image,
+			});
+			toast.promise(res, {
+				loading: { title: "Updating..." },
+				success: { title: "Updated successfully" },
+				error: { title: "Error updating" },
+			});
+			const response = await res;
+			console.log(response);
+			setUserData({
+				...userData,
+				name: response.data.name,
+				about: response.data.about,
+				image: response.data.image,
+			});
+
+			// Update the local storage
+			const user = JSON.parse(localStorage.getItem("user"));
+			user.name = response.data.name;
+			user.image = response.data.image;
+			localStorage.setItem("user", JSON.stringify(user));
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
 		<>
 			<div
 				// A button to go back to the home page
@@ -84,7 +164,83 @@ function Profile({handleToggle, toggle, user}) {
 							<Button
 								text="Edit Profile"
 								className="bg-slate-400 font-semibold p-2 border-1 rounded hover:bg-slate-500 hover:text-white transition-all duration-300 ease-in-out"
+								handleOnClick={onOpen}
 							/>
+							{/* Modal */}
+							<Modal isOpen={isOpen} onClose={onClose}>
+								<ModalOverlay />
+								<ModalContent>
+									<ModalHeader>Edit Profile</ModalHeader>
+									<ModalCloseButton />
+									<ModalBody>
+										{/* Image */}
+										<label htmlFor="image_uploads" className="cursor-pointer">
+											{previewImage ? (
+												<img
+													className="w-24 h-24 rounded-full mx-auto"
+													src={previewImage}
+												/>
+											) : (
+												<BsPersonCircle className="w-24 h-24 rounded-full my-[20px] mx-auto shadow-[0_0_15px_yellow]" />
+											)}
+										</label>
+										<input
+											className="hidden"
+											type="file"
+											name="image"
+											id="image_uploads"
+											accept=".jpg, .jpeg, .png, .svg"
+											onChange={getImage}
+										/>
+										{/* Name */}
+										<label htmlFor="name" className="text-sm">
+											Name
+										</label>
+										<input
+											type="text"
+											name="name"
+											id="name"
+											value={userData.name}
+											onChange={handleUserInput}
+											className="w-full p-2 border-1 rounded  outline"
+										/>
+										{/* Email but disabled */}
+										<label htmlFor="email" className="text-sm">
+											Email
+										</label>
+										<input
+											type="email"
+											name="email"
+											id="email"
+											value={userData.email}
+											disabled
+											className="w-full p-2 border-1 rounded"
+										/>
+										{/* About (max character 500) */}
+										<label htmlFor="about" className="text-sm">
+											About
+										</label>
+										<textarea
+											name="about"
+											id="about"
+											value={userData.about}
+											onChange={handleUserInput}
+											className="w-full p-2 border-1 rounded outline resize-none h-24"
+										/>
+									</ModalBody>
+									<ModalFooter>
+										<Button
+											text="Save"
+											className="bg-slate-400 font-semibold p-2 border-1 rounded hover:bg-slate-500 hover:text-white transition-all duration-300 ease-in-out"
+											handleOnClick={() => {
+												handleEdit();
+												onClose();
+											}}
+										/>
+									</ModalFooter>
+								</ModalContent>
+							</Modal>
+
 							<Button
 								text="Logout"
 								className="bg-slate-400 font-semibold p-2 border-1 rounded hover:bg-slate-500 hover:text-white transition-all duration-300 ease-in-out"
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,176 +1,220 @@
-const User = require('../models/User');
-const {StatusCodes} = require('http-status-codes');
-const CustomError = require('../errors');
-const { checkPermissions } = require('../utils');
-const cloudinary = require('cloudinary').v2;
-const Note = require('../models/Note');
-const mongoose = require('mongoose');
-const {sendEmail} = require('../utils');
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { checkPermissions } = require("../utils");
+const cloudinary = require("cloudinary").v2;
+const Note = require("../models/Note");
+const mongoose = require("mongoose");
+const { sendEmail } = require("../utils");
 
-const getAllUsers = async(req,res)=>{
-    const users= await User.find({}).select('-password').populate({path:'followers following',select:'name image'}).populate({ path : 'likes' , select : 'title description'}); 
-    res.status(StatusCodes.OK).json({users,count:users.length});
-}
+const getAllUsers = async (req, res) => {
+	const users = await User.find({})
+		.select("-password")
+		.populate({ path: "followers following", select: "name image" })
+		.populate({ path: "likes", select: "title description" });
+	res.status(StatusCodes.OK).json({ users, count: users.length });
+};
 
-const getSingleUser = async(req,res)=>{
-    const {id}=req.params;
-    // dono followers aur following ko use kar skta hu path mein space deke bas
-    // unneccessary space dene se error aa rha 'following ' ke end mein
-    const user = await User.findOne({_id:id}).select('-password').populate({path:'followers following',select:'name image'}).populate({ path : 'likes' , select : 'title description'}); 
-    // another way, don't even have to make a diff populate statment in schema
-    
-    res.status(StatusCodes.OK).json({user});
-}   
+const getSingleUser = async (req, res) => {
+	const { id } = req.params;
+	// dono followers aur following ko use kar skta hu path mein space deke bas
+	// unneccessary space dene se error aa rha 'following ' ke end mein
+	const user = await User.findOne({ _id: id })
+		.select("-password")
+		.populate({ path: "followers following", select: "name image" })
+		.populate({ path: "likes", select: "title description" });
+	// another way, don't even have to make a diff populate statment in schema
 
-const getCurrentUser = async(req,res)=>{
-    const {userId} = req.user;
+	res.status(StatusCodes.OK).json({ user });
+};
 
-    const user = await User.findOne({_id:userId}).select('-password').populate({
-        path : 'followers following',
-        select : 'name image'
-    }).populate({
-        path:'likes',
-        select : 'title description',
-    }).populate({
-        path:'sharedNotes',
-        select : 'title description content user category likes likedBy tags visibility sharedWith'
-    });
-    // later also have to give some more data including notes and may be setting pipeline too.. -> done
-    //name,image,about me of user ->done
-    // About note :  title,description,name,content,status-> done
-    // followers aur following vgera bhi dena hai ->done
-    // background image->done
+const getCurrentUser = async (req, res) => {
+	const { userId } = req.user;
 
-    // const notes=await Note.find({user:userId}).sort({createdAt:-1}).populate('user',['name','image']).populate('likedBy',['name','image']).populate('comments.user',['name','image']).populate('comments.replies.user',['name','image']).populate('comments.replies.replies.user',['name','image']);
-    const notes=await Note.find({user:userId}).populate('comments',['name','user','comment']).populate({
-        path:'likedBy',
-        select : 'name image',
-    });
+	const user = await User.findOne({ _id: userId })
+		.select("-password")
+		.populate({
+			path: "followers following",
+			select: "name image",
+		})
+		.populate({
+			path: "likes",
+			select: "title description",
+		})
+		.populate({
+			path: "sharedNotes",
+			select:
+				"title description content user category likes likedBy tags visibility sharedWith",
+		});
+	// later also have to give some more data including notes and may be setting pipeline too.. -> done
+	//name,image,about me of user ->done
+	// About note :  title,description,name,content,status-> done
+	// followers aur following vgera bhi dena hai ->done
+	// background image->done
 
-    const stats = await Note.aggregate([
-        {$match:{user:mongoose.Types.ObjectId(userId)}},
-        {
-            $group:{
-                _id:null,// '$user' -> error has match only for one user
-                numOfLikes:{$sum:'$likes'},
-            }
-        }
-    ])
-    // console.log(stats[0]?.numOfLikes);// was giving error if no notes were there
-    let noteLikes=0;
-    if(stats.length!==0){
-        noteLikes=stats[0].numOfLikes;
-    }
-    // console.log(noteLikes);
+	// const notes=await Note.find({user:userId}).sort({createdAt:-1}).populate('user',['name','image']).populate('likedBy',['name','image']).populate('comments.user',['name','image']).populate('comments.replies.user',['name','image']).populate('comments.replies.replies.user',['name','image']);
+	const notes = await Note.find({ user: userId })
+		.populate("comments", ["name", "user", "comment"])
+		.populate({
+			path: "likedBy",
+			select: "name image",
+		});
 
-    res.status(StatusCodes.OK).json({user,notes,numOfNotes:notes.length,noteLikes});
-} 
+	const stats = await Note.aggregate([
+		{ $match: { user: mongoose.Types.ObjectId(userId) } },
+		{
+			$group: {
+				_id: null, // '$user' -> error has match only for one user
+				numOfLikes: { $sum: "$likes" },
+			},
+		},
+	]);
+	// console.log(stats[0]?.numOfLikes);// was giving error if no notes were there
+	let noteLikes = 0;
+	if (stats.length !== 0) {
+		noteLikes = stats[0].numOfLikes;
+	}
+	// console.log(noteLikes);
 
-const updateUser = async(req,res)=>{
-    // name,email and image only
-    const {name,image,about} = req.body;
-    if(!name && !image && !about){
-        throw new CustomError.BadRequestError("Please Update name or image or about");
-    }
+	res
+		.status(StatusCodes.OK)
+		.json({ user, notes, numOfNotes: notes.length, noteLikes });
+};
 
-    const user = await User.findOne({_id:req.user.userId}).select('-password');
+const updateUser = async (req, res) => {
+	// name,email and image only
+	const { name, image, about } = req.body;
+	if (!name && !image && !about) {
+		throw new CustomError.BadRequestError(
+			"Please Update name or image or about"
+		);
+	}
 
-    if (image) {
-        function getImagePublicId(cloudinaryUrl) {
-            // Split the URL by '/'
-            const parts = cloudinaryUrl.split('/');
-            // Find the part that contains the public ID
-            const publicIdPart = parts[parts.length - 1].split('.')[0];
-            return publicIdPart;
-        }
-    
-        // Example usage
-        const cloudinaryUrl = user.image;
-        const imagePublicId = getImagePublicId(cloudinaryUrl);
-        console.log(imagePublicId); // Output: image_public_id
-        const result = await cloudinary.uploader.destroy(imagePublicId)
-            .then(result => {
-                console.log('Deleted success:', result);
-            })
-            .catch(error => {
-                console.error('Error deleting:', error);
-            });
-    
-        await cloudinary.api.delete_resources([`Pen-and-Pixel/${imagePublicId}`], { type: 'upload', resource_type: 'image' })
-            .then(result => {
-                console.log('Deleted resources:', result);
-            })
-            .catch(error => {
-                console.error('Error deleting resources:', error);
-            });
-    
-        user.image = image;
-    }
-    if(name){
-        user.name=name;
-    }
-    if(about){
-        user.about=about;
-    }
-    await user.save();
+	const user = await User.findOne({ _id: req.user.userId }).select("-password");
 
-    res.status(StatusCodes.OK).json({msg:"Successfully updated the user",user});
-}
+	if (image) {
+		if (image === "https://www.gravatar.com/avatar/000?d=mp") {
+			user.image = image;
+		} else {
+			function getImagePublicId(cloudinaryUrl) {
+				// Split the URL by '/'
+				const parts = cloudinaryUrl.split("/");
+				// Find the part that contains the public ID
+				const publicIdPart = parts[parts.length - 1].split(".")[0];
+				return publicIdPart;
+			}
 
-const deleteUser = async(req,res)=>{
-    const{id} = req.params;  
-    checkPermissions(req.user,id);
-    const user = await User.findOne({_id:id});
-    if(!user){
-        throw new CustomError.BadRequestError(`No user exists with id : ${id}`);
-    }
-    console.log("Delete user called");
-    await user.remove(); // to delete the info at post remove hook, followers, updating lists of followers and following, rating,review,comments,etc.
+			// Example usage
+			const cloudinaryUrl = user.image;
+			const imagePublicId = getImagePublicId(cloudinaryUrl);
+			console.log(imagePublicId); // Output: image_public_id
+			const result = await cloudinary.uploader
+				.destroy(imagePublicId)
+				.then((result) => {
+					console.log("Deleted success:", result);
+				})
+				.catch((error) => {
+					console.error("Error deleting:", error);
+				});
 
-    // Also deleting the notes of the user, ratings, comments,etc.
-    res.status(StatusCodes.OK).json({msg:"Successfully deleted the user"});
-}
+			await cloudinary.api
+				.delete_resources([`Pen-and-Pixel/${imagePublicId}`], {
+					type: "upload",
+					resource_type: "image",
+				})
+				.then((result) => {
+					console.log("Deleted resources:", result);
+				})
+				.catch((error) => {
+					console.error("Error deleting resources:", error);
+				});
 
-const updateUserPassword = async(req,res)=>{
-    const {userId} = req.user;
-    const {oldPassword,newPassword} = req.body;
+			user.image = image;
+		}
+	}
+	if (name) {
+		user.name = name;
+	}
+	if (about) {
+		user.about = about;
+	}
+	await user.save();
 
-    if(!oldPassword || !newPassword){
-        throw new CustomError.BadRequestError(`Please provide oldPassword and newPassword`);
-    }
+	res
+		.status(StatusCodes.OK)
+		.json({ msg: "Successfully updated the user", user });
+};
 
-    if(oldPassword===newPassword){
-        throw new CustomError.BadRequestError("New password can't be same as old password");
-    }
-    const user = await User.findOne({_id:userId});
-    const isPasswordCorrect = await user.comparePassword(oldPassword);
-    if(!isPasswordCorrect){
-        throw new CustomError.UnauthenticatedError("Invalid Password");
-    }
-    user.password = newPassword;
-    await user.save();
-    res.status(StatusCodes.OK).json({msg:"Successfully updated the password"});
-}
+const deleteUser = async (req, res) => {
+	const { id } = req.params;
+	checkPermissions(req.user, id);
+	const user = await User.findOne({ _id: id });
+	if (!user) {
+		throw new CustomError.BadRequestError(`No user exists with id : ${id}`);
+	}
+	console.log("Delete user called");
+	await user.remove(); // to delete the info at post remove hook, followers, updating lists of followers and following, rating,review,comments,etc.
 
-const contactUs = async(req,res)=>{
-    //to,html,subject
-    const {name,email,phone,message}= req.body;
+	// Also deleting the notes of the user, ratings, comments,etc.
+	res.status(StatusCodes.OK).json({ msg: "Successfully deleted the user" });
+};
 
-    if(!name || !email || !phone || !message){
-        throw new CustomError.BadRequestError("Please provide all the fields");
-    }
+const updateUserPassword = async (req, res) => {
+	const { userId } = req.user;
+	const { oldPassword, newPassword } = req.body;
 
-    const html = `
+	if (!oldPassword || !newPassword) {
+		throw new CustomError.BadRequestError(
+			`Please provide oldPassword and newPassword`
+		);
+	}
+
+	if (oldPassword === newPassword) {
+		throw new CustomError.BadRequestError(
+			"New password can't be same as old password"
+		);
+	}
+	const user = await User.findOne({ _id: userId });
+	const isPasswordCorrect = await user.comparePassword(oldPassword);
+	if (!isPasswordCorrect) {
+		throw new CustomError.UnauthenticatedError("Invalid Password");
+	}
+	user.password = newPassword;
+	await user.save();
+	res.status(StatusCodes.OK).json({ msg: "Successfully updated the password" });
+};
+
+const contactUs = async (req, res) => {
+	//to,html,subject
+	const { name, email, phone, message } = req.body;
+
+	if (!name || !email || !phone || !message) {
+		throw new CustomError.BadRequestError("Please provide all the fields");
+	}
+
+	const html = `
         <p>Name: ${name}</p>
         <p>Email: ${email}</p>
         <p>Phone: ${phone}</p>
         <p>Message: ${message}</p>
     `;
 
-    await sendEmail({to:'uncannydevs@gmail.com',html,subject:'Contact Us Form Submission'});
+	await sendEmail({
+		to: "uncannydevs@gmail.com",
+		html,
+		subject: "Contact Us Form Submission",
+	});
 
-    res.status(StatusCodes.OK).json({msg:"Email sent successfully"});
-}
+	res.status(StatusCodes.OK).json({ msg: "Email sent successfully" });
+};
 
-module.exports={getAllUsers,getSingleUser,getCurrentUser,updateUser,deleteUser,updateUserPassword,contactUs};
+module.exports = {
+	getAllUsers,
+	getSingleUser,
+	getCurrentUser,
+	updateUser,
+	deleteUser,
+	updateUserPassword,
+	contactUs,
+};
 // getAllUsers getSingleUser getCurrentUser(along with notes adding at later stage) updateUser deletUser updateUserPassword
